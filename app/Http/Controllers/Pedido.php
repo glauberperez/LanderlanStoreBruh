62d9b38bf82d84b83aa0ec845279613b0485c0af
Athirson <Athirsonarceus@gmail.com>
2022-09-26 15:18:58 -0300

    fak

--- app/Http/Controllers/Pedido.php
+++ app/Http/Controllers/Pedido.php
@@ -69,7 +69,7 @@ class Pedido extends Controller
 
     /**
      * Show the form for editing the specified resource.
-     *
+     *'
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
